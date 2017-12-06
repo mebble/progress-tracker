33240a243a158043c814ec180cb4582be2aa4ad0
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-12-06 22:36:00 +0530

    commit initial server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+const server = app.listen(port, () => {
+	console.log(`Server listening at port ${port}`);
+});
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // support encoded bodies
+app.use(express.static('public'));
+
+app.post('/add/day-progress', (req, res) => {
+	console.log(JSON.stringify(req.body, null, 4));
+	res.end();
+});
